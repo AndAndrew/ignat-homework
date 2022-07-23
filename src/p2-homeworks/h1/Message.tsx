9203eb917c9e92e6ec560521ecf13891a06a91a3
andAndrew <andAndrew.k@gmail.com>
2022-07-23 13:13:45 +0400

    feat: added html markup for message block

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-function Message() {
+export type MessageData = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+function Message(props: MessageData) {
     return (
         <div>
-
+            <div><img src={props.avatar}/></div>
+            <div>
+                <div>
+                    <div>{props.name}</div>
+                    <div>{props.message}</div>
+                </div>
+                <div>
+                    {props.time}
+                </div>
+            </div>
         </div>
     )
 }
